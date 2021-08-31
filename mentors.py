3ec562674e7276f697e2f234e9c54028fbc75e36
Ruslan Gusmanov <rururu@bk.ru>
2021-08-31 10:49:19 +0300

    Задание 1 Добавил классы Lecturer и Reviewer Разбил классы по отдельным файлам

---
+++ mentors.py
@@ -0,0 +1,26 @@
+import students
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student,
+                      students.Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    pass
