7db7fd7050328eb15804e803e8b191cc2b201733
Ruslan Gusmanov <rururu@bk.ru>
2021-08-31 11:26:55 +0300

    Задание 2 Добавил:  1. Для Reviewer`a возможность оценки студента  2. Для Student`a возможность оценки лектора

--- mentors.py
+++ mentors.py
@@ -7,20 +7,24 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def attach_course(self, course_name):
+        self.courses_attached.append(course_name)
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student,
-                      students.Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, students.Student) \
+                and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
--- students.py
+++ students.py
@@ -1,3 +1,6 @@
+import mentors
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -9,3 +12,20 @@ class Student:
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+
+    def add_courses_in_progress(self, course_name):
+        self.courses_in_progress.append(course_name)
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if grade < 0 or grade > 10:
+            return 'Ошибка'
+        if isinstance(lecturer, mentors.Lecturer) \
+                and course in lecturer.courses_attached \
+                and course in self.courses_in_progress \
+                or course in self.finished_courses:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
